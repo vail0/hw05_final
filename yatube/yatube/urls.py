b86915a36dffcdf39a15f212e6a2fec7c7b6fc16
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-21 04:50:52 +0300

    403and 500 error pages

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+handler500 = 'core.views.server_error'
 
 # Эти строки — в самый конец файла:
 if settings.DEBUG:
